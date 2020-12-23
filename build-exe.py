18ddf52648f115a67e26474528ec08e221eb3316
Chris Norman <chris.norman2@googlemail.com>
2020-12-23 12:26:53 +0000

    Remove spec file after build.

--- build-exe.py
+++ build-exe.py
@@ -1,15 +1,18 @@
 """A script for building an exe."""
 
+from pathlib import Path
 from datetime import datetime
 from subprocess import call
 
 if __name__ == "__main__":
     call(['rm', '-rf', 'build', 'dist', '*.spec'])
     now: datetime = datetime.now()
+    name: str = 'synthizer-reverb-%s.exe' % now.strftime('%Y-%m-%d')
     call(
         [
-            'pyinstaller', '--onefile', '-w', '--clean', '-y', '-n',
-            'synthizer-reverb-%s.exe' % now.strftime('%Y-%m-%d'),
+            'pyinstaller', '--onefile', '-w', '--clean', '-y', '-n', name,
             'synthizer-reverb.py'
         ]
     )
+    p: Path = Path(name + '.spec')
+    p.unlink()
